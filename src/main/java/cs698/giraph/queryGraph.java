17da0a4cd0664f7465ca908ae6181560a09ea7a5
nosrepus <nosrepus@gmail.com>
2016-04-06 17:29:47 -0400

    query graph

---
+++ src/main/java/cs698/giraph/queryGraph.java
@@ -0,0 +1,35 @@
+package cs698.giraph;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class queryGraph {
+    HashMap<Long, queryGraphVertex> graph;
+
+    queryGraph(){}
+    //todo
+    void build(){
+        for (Map.Entry<Long, queryGraphVertex> pair : graph.entrySet()) {
+            for (Long dest:pair.getValue().outNode) {
+                graph.get(dest).inNode.add(pair.getKey());
+            }
+        }
+    }
+
+    void insert(Long source, Long dest){
+        if(graph.containsKey(source)){
+            graph.get(source).outNode.add(dest);
+        }
+        else{
+            graph.put(source, new queryGraphVertex(dest));
+        }
+    }
+}
+
+public class queryGraphVertex {
+    public Set<Long> inNode;
+    public Set<Long> outNode;
+    public Long id;
+    queryGraphVertex(Long nodeId){id = nodeId;}
+}
