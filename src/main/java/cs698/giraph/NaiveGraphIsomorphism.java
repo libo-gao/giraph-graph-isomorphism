b9dca8a6c82db0528bc6832afa084f9ad264a9c5
nosrepus <nosrepus@gmail.com>
2016-04-08 12:34:43 -0400

    temp ver

--- src/main/java/cs698/giraph/NaiveGraphIsomorphism.java
+++ src/main/java/cs698/giraph/NaiveGraphIsomorphism.java
@@ -9,8 +9,10 @@ import org.apache.giraph.edge.Edge;
 import org.apache.giraph.graph.BasicComputation;
 import org.apache.giraph.graph.GraphState;
 import org.apache.giraph.graph.Vertex;
+import org.apache.giraph.utils.*;
 import org.apache.giraph.worker.WorkerGlobalCommUsage;
 import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.ArrayWritable;
 /*
 * Type Parameters:
 * I - Vertex id
@@ -19,14 +21,26 @@ import org.apache.hadoop.io.*;
 * M - Message type
 */
 
-public class NaiveGraphIsomorphism extends BasicComputation<LongWritable, DoubleWritable, FloatWritable, DoubleWritable>{
+public class NaiveGraphIsomorphism extends BasicComputation<LongWritable, ArrayWritable, FloatWritable, DoubleWritable>{
 
 
+	//first superstep store the innode
 	@Override
-	public void compute(Vertex<LongWritable, DoubleWritable, FloatWritable> vertex, Iterable<DoubleWritable> messages) throws IOException{
+	public void compute(Vertex<LongWritable, ArrayWritable, FloatWritable> vertex, Iterable<DoubleWritable> messages) throws IOException{
 		//first superstep does some preprocessing
 		if(getSuperstep()==0){
-			((GraphIsomorphismWorkerContext)getWorkerContext()).getInVertex((Long)0);
+			Iterable<Edge<LongWritable, FloatWritable>> edges = vertex.getEdges();
+			for (Edge<LongWritable, FloatWritable> edge : edges) {
+				sendMessage(edge.getTargetVertexId(), new DoubleWritable((double)vertex.getId().get()));
+			}
+		}
+		else if(getSuperstep()==1){
+			LongWritable[] tmp = vertex.getValue().get();
+			
+		}
+		else if(getSuperstep()==2){
+			((GraphIsomorphismWorkerContext)getWorkerContext()).getInVertex(new Long(0));
+
 		}
 		else{
 			
