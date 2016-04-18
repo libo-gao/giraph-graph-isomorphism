a57aa3fded1711b5c1c0413370d83208a16cdc12
nosrepus <nosrepus@gmail.com>
2016-04-18 07:13:03 -0400

    ts

--- src/main/java/cs698/giraph/GraphIsomorphismWorkerContext.java
+++ src/main/java/cs698/giraph/GraphIsomorphismWorkerContext.java
@@ -78,14 +78,14 @@ public class GraphIsomorphismWorkerContext extends WorkerContext {
         graph_array.add(new PairOfLongs(new Long(4), new Long(0)));
         graph_array.add(new PairOfLongs(new Long(5), new Long(0)));
         */
-        /*
+
         //liner
         //1->2->3
         graph_array.add(new PairOfLongs(new Long(1), new Long(0)));
         graph_array.add(new PairOfLongs(new Long(2), new Long(0)));
         graph_array.add(new PairOfLongs(new Long(3), new Long(0)));
-        */
-
+        
+        /*
         //star
         //    2
         // 5<-1->3
@@ -93,6 +93,7 @@ public class GraphIsomorphismWorkerContext extends WorkerContext {
         graph_array.add(new PairOfLongs(new Long(1), new Long(0)));
         graph_array.add(new PairOfLongs(new Long(2), new Long(1)));
         graph_array.add(new PairOfLongs(new Long(3), new Long(0)));
+        */
     }
 
     Set<Long> getOutVertex(Long id){
