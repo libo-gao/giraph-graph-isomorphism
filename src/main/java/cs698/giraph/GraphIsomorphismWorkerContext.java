ab43e6a23df64a79f390055f7cbdcacef948d42f
nosrepus <nosrepus@gmail.com>
2016-04-18 04:19:36 -0400

    graph

--- src/main/java/cs698/giraph/GraphIsomorphismWorkerContext.java
+++ src/main/java/cs698/giraph/GraphIsomorphismWorkerContext.java
@@ -60,6 +60,7 @@ public class GraphIsomorphismWorkerContext extends WorkerContext {
         graph_array.add(new PairOfLongs(new Long(5), new Long(3)));
         graph_array.add(new PairOfLongs(new Long(2), new Long(0)));
         */
+        /*
         //1->2->3->4
         //2->5
         graph_array.add(new PairOfLongs(new Long(1), new Long(0)));
@@ -67,14 +68,14 @@ public class GraphIsomorphismWorkerContext extends WorkerContext {
         graph_array.add(new PairOfLongs(new Long(3), new Long(0)));
         graph_array.add(new PairOfLongs(new Long(4), new Long(2)));
         graph_array.add(new PairOfLongs(new Long(5), new Long(0)));
+        */
 
-        /*
         //circle
         //1->2->3->1
         graph_array.add(new PairOfLongs(new Long(1), new Long(0)));
         graph_array.add(new PairOfLongs(new Long(2), new Long(0)));
         graph_array.add(new PairOfLongs(new Long(3), new Long(0)));
-        */
+        
         /*
         //liner
         //1->2->3
